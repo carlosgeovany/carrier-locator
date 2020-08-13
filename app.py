05c7edf829dbbe847741a7434459e770b990067e
carlosgeovany <ccggppvv@hotmail.com>
2020-08-13 18:12:04 -0500

    add favicon and tab name

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from dash_extensions.snippets import send_data_frame
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = 'Carrier Locator'
 server = app.server
 
 #---------------------------------------------------------------
